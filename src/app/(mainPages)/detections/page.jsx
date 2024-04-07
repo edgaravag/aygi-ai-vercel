4f50a4574dbd21e7cfd2b150684ab06f259e4fc5
edgaravag <edgar.avagyan20006@gmail.com>
2024-04-07 20:27:58 +0400

    Finished detections page

--- src/app/(mainPages)/detections/page.jsx
+++ src/app/(mainPages)/detections/page.jsx
@@ -1,28 +1,74 @@
-import SingleDetection from "./_components/SingleDetection"
-import DetectionImage from "@public/plants/detectionImage.webp"
+import DeleteAllButton from "./_components/DeleteAllButton";
+import PrevAndNextPages from "./_components/PrevAndNextPages";
+import SingleDetection from "./_components/SingleDetection";
+import DetectionImage from "@public/plants/detectionImage.webp";
 
 const detections = [
-    { img: DetectionImage.src, name: "Plant Name", description: "Lorem Imspum ipsum Lorem", date: "08.10.2014" },
-    { img: DetectionImage.src, name: "Plant Name", description: "Lorem Imspum ipsum Lorem", date: "08.10.2014" },
-    { img: DetectionImage.src, name: "Plant Name", description: "Lorem Imspum ipsum Lorem", date: "08.10.2014" },
-    { img: DetectionImage.src, name: "Plant Name", description: "Lorem Imspum ipsum Lorem", date: "08.10.2014" },
-    { img: DetectionImage.src, name: "Plant Name", description: "Lorem Imspum ipsum Lorem", date: "08.10.2014" },
-    { img: DetectionImage.src, name: "Plant Name", description: "Lorem Imspum ipsum Lorem", date: "08.10.2014" },
-    { img: DetectionImage.src, name: "Plant Name", description: "Lorem Imspum ipsum Lorem", date: "08.10.2014" },
-    { img: DetectionImage.src, name: "Plant Name", description: "Lorem Imspum ipsum Lorem", date: "08.10.2014" }
-]
+  {
+    img: DetectionImage.src,
+    name: "Plant Name",
+    description: "Lorem Imspum ipsum Lorem",
+    date: "08.10.2014",
+  },
+  {
+    img: DetectionImage.src,
+    name: "Plant Name",
+    description: "Lorem Imspum ipsum Lorem",
+    date: "08.10.2014",
+  },
+  {
+    img: DetectionImage.src,
+    name: "Plant Name",
+    description: "Lorem Imspum ipsum Lorem",
+    date: "08.10.2014",
+  },
+  {
+    img: DetectionImage.src,
+    name: "Plant Name",
+    description: "Lorem Imspum ipsum Lorem",
+    date: "08.10.2014",
+  },
+  {
+    img: DetectionImage.src,
+    name: "Plant Name",
+    description: "Lorem Imspum ipsum Lorem",
+    date: "08.10.2014",
+  },
+  {
+    img: DetectionImage.src,
+    name: "Plant Name",
+    description: "Lorem Imspum ipsum Lorem",
+    date: "08.10.2014",
+  },
+  {
+    img: DetectionImage.src,
+    name: "Plant Name",
+    description: "Lorem Imspum ipsum Lorem",
+    date: "08.10.2014",
+  },
+  {
+    img: DetectionImage.src,
+    name: "Plant Name",
+    description: "Lorem Imspum ipsum Lorem",
+    date: "08.10.2014",
+  },
+];
 
 const Detections = () => {
   return (
-    <main className="w-full flex flex-col items-center  mx-auto px-[95px] py-9">
+    <main className="w-full px-[95px] py-9">
+      <div className="mx-auto flex flex-col w-full gap-4 max-w-[572px]">
       <h2 className="font-semibold text-lg">Detection History</h2>
-      <div className="flex flex-col w-full gap-4 mt-6 max-w-[572px]">
-          {detections.map((detection, index) => {
-              return <SingleDetection key={index} detection={detection} />
-          })}
+        {detections.map((detection, index) => {
+          return <SingleDetection key={index} detection={detection} />;
+        })}
+        <div className="flex items-center justify-between mt-9">
+          <DeleteAllButton />
+          <PrevAndNextPages />
+        </div>
       </div>
     </main>
-  )
-}
+  );
+};
 
-export default Detections
+export default Detections;
