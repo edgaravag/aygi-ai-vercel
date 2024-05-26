fbe5fa1aea5031a8abce1598fbfb7f5165c36831
edgaravag <edgar.avagyan20006@gmail.com>
2024-05-26 20:56:22 +0400

    Some fix

--- src/app/(mainPages)/layout.jsx
+++ src/app/(mainPages)/layout.jsx
@@ -1,56 +1,71 @@
-'use client';
-import RightAside from './_components/rightAside/RightAside';
-import Header from './_components/header/Header';
-import LeftAside from './_components/leftAside/LeftAside';
-import dynamic from 'next/dynamic';
-import { usePathname, useRouter } from 'next/navigation';
-import { useDispatch } from 'react-redux';
-import { setUserData } from '@/src/store/features/userDataSlice/userDataSlice';
-import { useEffect } from 'react';
-import axiosInstance from '@/src/utils/axiosInstance';
+"use client";
+import RightAside from "./_components/rightAside/RightAside";
+import Header from "./_components/header/Header";
+import LeftAside from "./_components/leftAside/LeftAside";
+import dynamic from "next/dynamic";
+import { usePathname, useRouter } from "next/navigation";
+import { useDispatch } from "react-redux";
+import { setUserData } from "@/src/store/features/userDataSlice/userDataSlice";
+import { useEffect, useLayoutEffect, useState } from "react";
+import axiosInstance from "@/src/utils/axiosInstance";
 // import { cookies } from 'next/headers';
-const Footer = dynamic(() => import('../_components/footer/Footer'));
+const Footer = dynamic(() => import("../_components/footer/Footer"));
 
 export default function RootLayout({ children }) {
+  const [accessToken, setAccessToken] = useState(null);
   const router = usePathname();
   const path = useRouter();
 
-  const accessToken = localStorage.getItem('accessToken');
+  useLayoutEffect(() => {
+    const token = localStorage.getItem("accessToken");
+    setAccessToken(token);
+  }, [accessToken]);
+
+  // console.log(accessToken);
+
+  useEffect(() => {
+    const token = localStorage.getItem("accessToken");
+    if (!token) {
+      // console.log("useEffect" + token);
+      window.location.href = "/";
+    }
+  }, [accessToken, router]);
+
   // const cookiesStore = cookies();
   // const JSESSIONID = cookiesStore.get('JSESSIONID');
-  // console.log(JSESSIONID + '----------------------------------------');
+  // console.log(JSESSIONID + '----');
 
   const dispatch = useDispatch();
 
   useEffect(() => {
     axiosInstance
-      .get('/user/')
+      .get("/user/")
       .then((response) => {
         dispatch(setUserData(response.data));
       })
       .catch((error) => {
-        console.error('Error fetching user data:', error);
+        console.error("Error fetching user data:", error);
       });
   }, [dispatch]);
 
   if (accessToken) {
     return (
-      <div className='bg-[#F7F7F7]'>
+      <div className="bg-[#F7F7F7]">
         <Header />
-        <div className='w-full flex pt-[104px]'>
+        <div className="w-full flex pt-[104px]">
           <LeftAside path={router} />
-          <main className='px-8 py-9 mx-auto'>{children}</main>
-          {router.includes('/faq/questions') ||
-          router.includes('/library') ? null : (
+          <main className="px-8 py-9 mx-auto">{children}</main>
+          {router.includes("/faq/questions") ||
+          router.includes("/library") ? null : (
             <RightAside />
           )}
         </div>
-        {router.includes('/faq/questions') || router.includes('/library') ? (
+        {router.includes("/faq/questions") || router.includes("/library") ? (
           <Footer />
         ) : null}
       </div>
     );
   } else {
-    path.push('/');
+    return null;
   }
 }
