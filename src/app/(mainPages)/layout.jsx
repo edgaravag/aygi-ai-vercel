4c1a7d0ffb1137f00e135a0a9c38633c88346a70
Artyom7577 <artyom.hayrapetyan0001@gmail.com>
2024-05-26 16:50:25 +0400

    google api

--- src/app/(mainPages)/layout.jsx
+++ src/app/(mainPages)/layout.jsx
@@ -5,27 +5,31 @@ import LeftAside from './_components/leftAside/LeftAside';
 import dynamic from 'next/dynamic';
 import { usePathname, useRouter } from 'next/navigation';
 import { useDispatch } from 'react-redux';
-import { setUserData } from "@/src/store/features/userDataSlice/userDataSlice";
-import { useEffect } from "react";
-import axiosInstance from "@/src/utils/axiosInstance";
+import { setUserData } from '@/src/store/features/userDataSlice/userDataSlice';
+import { use, useEffect, useState } from 'react';
+import axiosInstance from '@/src/utils/axiosInstance';
+// import { cookies } from 'next/headers';
 const Footer = dynamic(() => import('../_components/footer/Footer'));
 
 export default function RootLayout({ children }) {
   const router = usePathname();
   const path = useRouter();
 
-  const accessToken = localStorage.getItem('accessToken');
+  // const accessToken = localStorage.getItem('accessToken');
+  // const cookiesStore = cookies();
+  // const JSESSIONID = cookiesStore.get('JSESSIONID');
+  // console.log(JSESSIONID + '----------------------------------------');
 
   const dispatch = useDispatch();
 
   useEffect(() => {
     axiosInstance
-      .get("/user/")
+      .get('/user/')
       .then((response) => {
         dispatch(setUserData(response.data));
       })
       .catch((error) => {
-        console.error("Error fetching user data:", error);
+        console.error('Error fetching user data:', error);
       });
   }, [dispatch]);
 
