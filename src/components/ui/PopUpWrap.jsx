01c767ce30fb0b57be1779392dc9ecac261a8e42
edgaravag <edgar.avagyan20006@gmail.com>
2024-05-27 15:19:18 +0400

    Popup style fix

--- src/components/ui/PopUpWrap.jsx
+++ src/components/ui/PopUpWrap.jsx
@@ -4,14 +4,14 @@ import Image from "next/image";
 const PopUpWrap = ({ onClose, children, className }) => {
   return (
     <div className="center z-[1000] bg-black bg-opacity-40 fixed top-0 left-0 right-0 bottom-0 h-screen">
-      <div className="mx-10">
-        <div className={`relative bg-white rounded-md ${className}`}>
-          <button
-            className="absolute z-[1000] center w-[41px] h-[41px] top-[-18px] right-[-18px] bg-[#68bb59] rounded-full overflow-hidden"
-            onClick={() => onClose()}
-          >
-            <Image src={CloseIcon} alt="Close Icon" />
-          </button>
+      <div className={`relative bg-white rounded-md mx-5 ${className}`}>
+        <button
+          className="absolute z-[1000] center w-[41px] h-[41px] top-[-18px] right-[-18px] bg-[#68bb59] rounded-full overflow-hidden"
+          onClick={() => onClose()}
+        >
+          <Image src={CloseIcon} alt="Close Icon" />
+        </button>
+        <div className="w-full">
           {children}
         </div>
       </div>
