17c1e4dec06a7703b6fcc99dd810058227d7802a
dfgh <hovsepbabayan1@mail.ru>
2024-06-01 09:38:57 +0200

    test

--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -1,13 +1,23 @@
 import React from 'react';
 import Image from 'next/image';
+import Link from 'next/link';
 import NotFoundImage from '@public/plants/notfound.webp';
+import Footer from './_components/footer/Footer';
 
 const NotFound = () => {
   return (
     <>
-      <div className="mx-auto max-w-max">
-        <Image src={NotFoundImage} width={988} height={817} alt="Not Found" />
+      <div className="container mx-auto px-4 sm:px-8 md:px-16 lg:px-32 xl:px-56 2xl:px-72 text-center">
+        <Image src={NotFoundImage} layout="responsive" width={536} height={443} alt="Not Found" />
+        <div className="mt-8 flex justify-center">
+          <Link href="/mygarden" passHref>
+            <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500">
+              Go to My Garden
+            </button>
+          </Link>
+        </div>
       </div>
+      <Footer />
     </>
   );
 };
