72a3374cb55d5d231828c9ee643cdda2131a9d60
dfgh <hovsepbabayan1@mail.ru>
2024-06-08 19:55:45 +0200

    make detection button

--- src/components/modals/MakeDetection/MakeDetection.jsx
+++ src/components/modals/MakeDetection/MakeDetection.jsx
@@ -2,19 +2,22 @@ import { useState } from "react";
 import Modal from "../../ui/Modal";
 import PlantIdentification from "./components/PlantIdentification";
 import DiseaseDetection from "@/src/components/modals/MakeDetection/components/DiseaseDetection";
+import Dropdown from "./components/Dropdown";  // Adjust the path as necessary
 
 const MakeDetection = ({ onClose, setShowDetectionResult }) => {
-  const [option, setOption] = useState(true);
+  const [option, setOption] = useState("disease");
+
+  const handleDropdownChange = (value) => {
+    setOption(value);
+  };
 
   return (
     <Modal onClose={onClose} className="p-8">
-      <button className="flex gap-4 mb-4" onClick={() => setOption(!option)}>
-        Choose{" "}
-        <p className="font-medium">
-          {option ? "Disease detection" : "Plant identification"}
-        </p>
-      </button>
-      {option ? (
+      <div className="flex gap-4 mb-4 items-center"> {/* Added items-center */}
+        <span>Choose</span>
+        <Dropdown selectedOption={option} onChange={handleDropdownChange} />
+      </div>
+      {option === "plant" ? (
         <PlantIdentification
           setShowDetectionResult={setShowDetectionResult}
           onClose={onClose}
